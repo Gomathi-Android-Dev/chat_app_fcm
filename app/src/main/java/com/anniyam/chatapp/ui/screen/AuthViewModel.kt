340e827b20cfcb582bde4cbe4df8c6f0a18d3e71
Gomathi-at-899069454587 <gomathi@anniyam.com>
2026-04-01 15:48:59 +0530

    viewmodel

--- app/src/main/java/com/anniyam/chatapp/ui/screen/AuthViewModel.kt
+++ app/src/main/java/com/anniyam/chatapp/ui/screen/AuthViewModel.kt
@@ -10,7 +10,7 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 
-@HiltViewModel
+
 class AuthViewModel @Inject constructor() : ViewModel() {
 
     private val auth: FirebaseAuth = FirebaseAuth.getInstance()
